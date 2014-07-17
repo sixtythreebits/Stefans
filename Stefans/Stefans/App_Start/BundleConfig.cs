2c3c0393d4e79a7aef92ca9283ff2eb60a159cd8
Tornike Kharaishvili <tornike.kharishvili@63bits.com>
2014-07-17 16:37:30 +0400

    Project setup

---
+++ Stefans/Stefans/App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Stefans
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection Bundles)
+        {
+            //Bundles.Add(new ScriptBundle("~/bundles/layout").Include(
+            //            "~/Scripts/jquery-{version}.js",
+            //            "~/Scripts/jquery-ui-{version}.js",
+            //            "~/Scripts/General/utils.js",
+            //            "~/Scripts/CM/layout.js",
+            //            "~/Scripts/CM/calendar.js",
+            //            "~/Scripts/CM/cart.js",
+            //            "~/Scripts/CM/player.js"));
+
+            //Bundles.Add(new ScriptBundle("~/bundles/validation").Include(
+            //    "~/Scripts/jquery.validate.js",
+            //    "~/Scripts/jquery.validate.unobtrusive.js"));
+
+            Bundles.Add(new StyleBundle("~/bundles/content").Include(
+                "~/Content/style.css",
+                "~/Content/fonts.css"));
+        }
+
+        public static IHtmlString PutLayoutStyle()
+        {
+            return Styles.Render("~/bundles/content");
+        }
+    }
+}
---
+++ Stefans/Stefans/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Stefans
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection Routes)
+        {
+            Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            Routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
