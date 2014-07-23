328adaf5887f28c10e6a16ce7f4d838a80d71cdc
Tornike Kharaishvili <tornike.kharishvili@63bits.com>
2014-07-23 18:23:50 +0400

    Product Entity

---
+++ Stefans/Core/CM/Product.cs
@@ -0,0 +1,26 @@
+﻿using DB;
+
+namespace Core.CM
+{
+    public class Product : GenericObjectBase<DBCoreDataContext>
+    {
+        #region Constructor
+
+        public Product() : base(ConnectionFactory.GetCoreContext)
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int ID { get; set; }
+        public string Caption { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public string Instruction { get; set; }
+
+        #endregion
+    }
+}
