956c9e68dfc44132418803651425ae7337421c7b
Tornike Kharaishvili <tornike.kharishvili@63bits.com>
2014-07-23 17:46:58 +0400

    Static content setup for administration

---
+++ Stefans/Stefans/Areas/Admin/Controllers/MainController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Stefans.Areas.Admin.Controllers
+{
+    public class MainController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
