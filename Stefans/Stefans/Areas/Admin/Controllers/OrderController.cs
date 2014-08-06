95f402ab42ad15d5c808cba530901446a4d37a78
Tornike Kharaishvili <tornike.kharishvili@63bits.com>
2014-08-06 17:50:56 +0400

    Missing files included

---
+++ Stefans/Stefans/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using Core.CM;
+
+namespace Stefans.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OrderGrid()
+        {
+            var model = new Order().GetList();
+            return PartialView("_OrderGrid", model);
+        }
+
+        public ActionResult Details(int ID)
+        {
+            var model = new OrderDetail().GetList(ID);
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(model);
+            }
+            return View(model);
+        }
+    }
+}
